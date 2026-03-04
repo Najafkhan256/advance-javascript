b386d9354dd8107ee144c2d5fdc238472b518047
Najafkhan256 <najafkhan6625@gmail.com>
2026-03-05 04:22:19 +0500

    template string

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const name = "John Doe";
 const age = 25;
 const greeting = "Hello, Advanced JavaScript!";
 
-console.log(name + " is " + age + " years old.");
+// console.log(name + " is " + age + " years old.");
+console.log(`${name} is ${age} years old.`);
