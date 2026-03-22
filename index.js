cc6335f68a0f0b864f24d29446507c95fea0ffb7
Najafkhan256 <najafkhan6625@gmail.com>
2026-03-23 04:07:02 +0500

    feat: fetch method

--- index.js
+++ index.js
@@ -1,41 +1,97 @@
-function promiseResolved(number, stage) {
-  return function (resolved, rejected) {
-    setTimeout(() => {
-      console.log(`${stage} Promise is ${resolved ? "resolved" : "rejected"}`);
-      resolved(number);
-    }, number * 100);
-  };
-}
+const titleEl = document.getElementById("title");
+const bodyEl = document.getElementById("body");
+const userIdEl = document.getElementById("userId");
+const formSubmitEl = document.getElementById("formSubmit");
+const postContainerEl = document.getElementById("post-list");
+const modalEl = document.getElementById("modal");
+const modalCloseButtonEl = document.getElementById("close-btn");
+const addUserBtnEl = document.getElementById("add-user-btn");
+const deleteUserBtnEl = document.getElementById("delete-user-btn");
+const modalTitleEl = document.getElementById("modal-title");
+const modalFromSubmitEl = document.getElementById("modalFormSubmit");
 
-let p1 = new Promise(promiseResolved(10, "First"));
-let p2 = new Promise(promiseResolved(20, "Second"));
-let p3 = new Promise(promiseResolved(30, "Third"));
-let p4 = new Promise((resolved, rejected) => {
-  rejected("4th promise is rejected!");
+let users = [];
+
+addUserBtnEl.addEventListener("click", function () {
+  modalEl.style.display = "flex";
+  modalTitleEl.innerText = "Add User";
+  titleEl.value = "";
+  bodyEl.value = "";
+  userIdEl.value = "";
 });
 
-// let p2 = new Promise((resolved, rejected) => {
-//   setTimeout(() => {
-//     console.log("Second Promise is rejected.");
-//     resolved(20);
-//   }, 2 * 1000);
-// });
-// let p3 = new Promise((resolved, rejected) => {
-//   setTimeout(() => {
-//     console.log("Third Promise is resolved.");
-//     resolved(30);
-//   }, 3 * 1000);
-// });
-
-let total = 0;
-Promise.all([p1, p2, p3, p4])
-  .then((success) => {
-    for (let i in success) {
-      total += success[i];
+let prom = new Promise((resolved, reject) => {
+  setTimeout(() => {
+    if (users.length > 0) {
+      resolved(users);
+    } else {
+      reject("No data found");
     }
-    console.log(`RESULT: ${success}`);
-    console.log(`TOTAL: ${total}`);
+  }, 3000);
+});
+
+console.log("prom:", prom);
+
+modalFromSubmitEl.addEventListener("click", function () {
+  fetch(`https://jsonplaceholder.typicode.com/posts/${index}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      title: userData.title,
+      body: userData.title,
+      userId: Number(userData.title),
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
   })
-  .catch((error) => {
-    console.log(`ERROR: ${error}`);
-  });
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+});
+
+function closeModal() {
+  modalEl.style.display = "none";
+}
+
+formSubmitEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: titleEl.value,
+      body: bodyEl.value,
+      userId: Number(userIdEl.value),
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      alert("Post added successfully!");
+      console.log(data);
+    });
+});
+
+function editList(index) {
+  modalEl.style.display = "flex";
+  const userData = users[index];
+  console.log(users[index]);
+
+  titleEl.value = userData.title;
+  bodyEl.value = userData.body;
+  userIdEl.value = userData.id;
+
+  form;
+}
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => response.json())
+  .then((data) => {
+    users = data;
+    for (let i in data) {
+      const item = data[i];
+      let message = `<div onclick="editList(${i})"> ${item.id} ${item.title} </div>`;
+      postContainerEl.innerHTML += message;
+    }
+  })
+  .catch((error) => console.log("Data not fetched", error));
